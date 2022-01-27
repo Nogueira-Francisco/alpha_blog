89d189d5af819852d2bbd2ff3aaa400ac74ad0ab
Francisco Nogueira <fdcnog@gmail.com>
2022-01-27 09:05:52 -0300

    Added edit and delete actions.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-    validates :title, presence: true,  length: { in: 6..10 }
+    validates :title, presence: true,  length: { in: 6..24 }
     validates :description, presence: true, length: { in: 10..360 }
 end
